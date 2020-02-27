31bac643cab2d408a11c328875f583ad801ed179
subaquatic-pierre <subaquatic.pierre@gmail.com>
2020-02-27 08:44:19 +0000

    update dive sites, send to local host

--- diving/import_website_pics.py
+++ diving/import_website_pics.py
@@ -4,16 +4,16 @@ import os
 from divesandybeach.settings import BASE_DIR
 from PIL import UnidentifiedImageError
 
-for file in os.listdir(os.path.join(BASE_DIR, 'test')):
+for file in os.listdir(os.path.join(BASE_DIR, 'website_images')):
     title = file.split('.')[0]
-    path = os.path.join(BASE_DIR, 'test', file)
+    path = os.path.join(BASE_DIR, 'website_images', file)
     with open(path, 'rb') as f:
         print('--- Opening file ---', title)
         try:
             pic = Images(title=title, image=File(f))
             pic.save()
-            print('--- Saved Picture ---', title)
-        except EOFError:
+            print('...\n--- Saved Picture ---')
+        except OSError:
             print('--- Wrong format, unable to save file ---', title)
         except UnidentifiedImageError:
             print('UnidentifiedImageError', title)
