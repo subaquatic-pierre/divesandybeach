6c9cdc64456f6701d326f5eb86b3b5dd1acc97a5
subaquatic-pierre <subaquatic.pierre@gmail.com>
2020-03-03 13:47:05 +0400

    fix template tag spelling

--- diving/templatetags/social_media.py
+++ diving/templatetags/social_media.py
@@ -6,7 +6,7 @@ register = template.Library()
 @register.simple_tag(takes_context=True)
 def og_image(context):
     try:
-        og_image_url = f"https://scubdaidivedubai.com{context['object'].image.image.url}"
+        og_image_url = f"https://scubadivedubai.com{context['object'].image.image.url}"
     except Exception:
         og_image_url = "https://scubadivedubai.com/static/core/img/Untitled-2_0006_IMG_5579-2.jpg"
 
