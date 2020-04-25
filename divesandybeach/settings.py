691069ddda764d7fc61dfa96c756d9e11e2ecd2a
subaquatic-pierre <subaquatic.pierre@gmail.com>
2020-04-25 14:00:39 +0400

    remote boto storage

--- divesandybeach/settings.py
+++ divesandybeach/settings.py
@@ -134,15 +134,6 @@ else:
 
 # All other settings
 
-# Django storages
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-AWS_ACCESS_KEY_ID = config.AWS_ACCESS_KEY_ID
-AWS_SECRET_ACCESS_KEY = config.AWS_SECRET_ACCESS_KEY
-AWS_STORAGE_BUCKET_NAME = config.AWS_STORAGE_BUCKET_NAME
-AWS_DEFAULT_ACL = None
-AWS_QUERYSTRING_AUTH = False
-
 # Timezone settings
 DATE_FORMAT = 'd m Y'
 LANGUAGE_CODE = 'en-us'
